918081c2345de83ae9d6e3a25cabba577e52adb8
Mikael <148342331+hookahmk@users.noreply.github.com>
2023-10-18 08:57:19 -0300

    first commit

---
+++ src/composables/index.js
@@ -0,0 +1,86 @@
+export const dataEssencia = [
+  {
+    title: 'Essência Ziggy Watermelon Bomb',
+    img: 'https://i.ibb.co/SKRCVPW/Ziggy-Watermelon-Bomb.jpg',
+    price: 13.0
+  },
+  {
+    title: 'Essência Ziggy Banana Tropical',
+    img: 'https://i.ibb.co/DrCtxp3/Ziggy-Banana-Tropical.jpg',
+    price: 13.0
+  },
+  {
+    title: 'Essência Mix ziggy Morango e Laranja',
+    img: 'https://i.ibb.co/CtyQ6GQ/Mix-ziggy-Morango-Laranja.jpg',
+    price: 13.0
+  },
+  {
+    title: 'Essência Ziggy Brasil do campo ao hexa',
+    img: 'https://i.ibb.co/cvR3b2n/ziggy-Brasil-do-campo-hexa.jpg',
+    price: 13.0
+  },
+  {
+    title: 'Essência Ziggy Yougurt',
+    img: 'https://i.ibb.co/qphj98B/Ziggy-Yougurt.jpg',
+    price: 13.0
+  },
+  {
+    title: 'Essência Ônix Banana e Açaí',
+    img: 'https://i.ibb.co/3Ms2xCc/Onix-Banana-a-ai.jpg',
+    price: 14.0
+  },
+  {
+    title: 'Essência Onix grape',
+    img: 'https://i.ibb.co/hf8pMmt/Onix-grape.jpg',
+    price: 14.0
+  },
+  {
+    title: 'Essência Ônix Danon',
+    img: 'https://i.ibb.co/ryJhWn7/Onix-Danon.jpg',
+    price: 14.0
+  },
+  {
+    title: 'Essência Ônix Red Drops',
+    img: 'https://i.ibb.co/GtYwsjQ/Onix-Red-Drops.jpg',
+    price: 14.0
+  },
+  {
+    title: 'Essência High Lemon Ônix',
+    img: 'https://i.ibb.co/xDhjWjv/High-Lemon-Onix.jpg',
+    price: 14.0
+  }
+]
+export const dataCarvoes = [
+  {
+    title: 'Carvão Chacal 250g',
+    img: 'https://i.ibb.co/JKDVqrH/Carvao-Chacal.jpg',
+    price: 15.0
+  },
+  {
+    title: 'Carvão Coco Bass 250g',
+    img: 'https://i.ibb.co/znqTWZj/Carvao-Coco-Bass.jpg',
+    price: 14.0
+  },
+  {
+    title: 'Carvão Avulso Chacal',
+    img: 'https://i.ibb.co/RNXtTtQ/Carvao-Avulso-Chacal.jpg',
+    price: 1.0
+  }
+]
+export const dataAluminio = [
+  {
+    title: 'Alumínio Stan',
+    img: 'https://i.ibb.co/fvVGkgp/Alum-nio-Stan.jpg',
+    price: 15.0
+  },
+  {
+    title: 'Alumínio Predator',
+    img: 'https://i.ibb.co/xHpV4cP/Alum-nio-Predator.jpg',
+    price: 15.0
+  },
+  {
+    title: 'Alumínio avulso brazucas',
+    img: 'https://i.ibb.co/kBN5mdq/Alum-nio-avulso-brazucas.jpg',
+    price: 1.0
+  }
+]
